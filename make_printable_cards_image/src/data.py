8e12287dbe7cdf54941ebfbba366174901218471
Gennady Terekhov <ter.ge@yandex.ru>
2023-07-29 17:51:04 +0300

    d

---
+++ make_printable_cards_image/src/data.py
@@ -0,0 +1,64 @@
+class Data:
+    def get_resources():
+        return {
+            "Fruit": {
+                "food": 2,
+                "trading_ability": 1,
+                "production": 0,
+                "culture": 0
+            },
+            "Lake": {
+                "food": 2,
+                "trading_ability": 0,
+                "production": 0,
+                "culture": 1
+            },
+            "Silver": {
+                "food": 0,
+                "trading_ability": 2,
+                "production": 0,
+                "culture": 1
+            },
+            "River": {
+                "food": 1,
+                "trading_ability": 2,
+                "production": 0,
+                "culture": 1
+            },
+            "Pasture": {
+                "food": 3,
+                "trading_ability": 0,
+                "production": 0,
+                "culture": 0
+            },
+            "Metal": {
+                "food": 0,
+                "trading_ability": 1,
+                "production": 2,
+                "culture": 0
+            },
+            "Forest": {
+                "food": 1,
+                "trading_ability": 0,
+                "production": 2,
+                "culture": 0
+            },
+            "Desert": {
+                "food": 0,
+                "trading_ability": 1,
+                "production": 0,
+                "culture": 1
+            },
+            "Stone": {
+                "food": 0,
+                "trading_ability": 0,
+                "production": 2,
+                "culture": 1
+            },
+            "Gold": {
+                "food": 0,
+                "trading_ability": 3,
+                "production": 0,
+                "culture": 0
+            }
+        }
